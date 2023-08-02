f68ef0d1a83098fca3b2d85a1a67d2243be2daf8
mark20306 <71766752+mark20306@users.noreply.github.com>
2023-08-02 23:40:41 +0800

    Time: 143 ms (99.1%), Space: 17.8 MB (96.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hash_table = {}
+        more = None
+        count = 0
         for i in nums:
-            hash_table[i]=hash_table.get(i , 0)+1
-            if hash_table[i]>(len(nums))//2:                
-                return i
+            if count == 0:
+                more = i
+                count = 1
+            elif i == more:
+                count += 1
+            else: 
+                count -= 1
+        return more    
