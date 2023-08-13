ea85001aad50d30402c30c627f492be904bcc3ee
mark20306 <71766752+mark20306@users.noreply.github.com>
2023-08-13 16:23:07 +0800

    Time: 36 ms (90.75%), Space: 16.1 MB (99.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,11 +4,13 @@ class Solution:
         stack = []
 
         for c in s:
-            if c not in Map:
-                stack.append(c)
-                continue
-            if not stack or stack[-1] != Map[c]:
-                return False
-            stack.pop()
+            if c in Map:
+                if stack and stack[-1] == Map[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)            
+                
 
         return not stack
