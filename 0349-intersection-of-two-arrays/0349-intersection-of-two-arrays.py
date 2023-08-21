4882d89a0d3f2553d3d7cda4648532cacf912bb5
mark20306 <71766752+mark20306@users.noreply.github.com>
2023-08-22 07:10:07 +0800

    Time: 65 ms (33.99%), Space: 16.6 MB (30.79%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -10,7 +10,7 @@ class Solution:
                 mid = (r - l) // 2 + l
                 if nums2[mid] == i and i not in res:
                     res.append(i)
-                    continue
+                    
                 elif nums2[mid] > i:
                     r = mid - 1
                 else:
