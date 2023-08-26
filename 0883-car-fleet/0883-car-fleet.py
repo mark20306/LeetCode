b317c9fa842b1c94c658e2c03773a87dff4ca801
mark20306 <71766752+mark20306@users.noreply.github.com>
2023-08-26 22:08:57 +0800

    Time: 858 ms (55.47%), Space: 36.4 MB (79.33%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -5,6 +5,6 @@ class Solution:
 
         for p , s in sorted(pair)[::-1]:
             stack.append((target - p) / s)
-            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+            if len(stack) > 1 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
