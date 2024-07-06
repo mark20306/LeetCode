889d960edfe418523569e16c4fcf89cc4b95d924
mark20306 <71766752+mark20306@users.noreply.github.com>
2024-07-07 04:22:00 +0800

    Time: 31 ms (90.93%), Space: 16.5 MB (91.06%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,14 +1,5 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        Map = {}
-        for i in s:
-            if i not in Map:
-                Map[i] = 1
-            else:
-                Map[i] += 1
-        
-        for c in t:
-            if c in Map and Map[c] > 0:
-                Map[c] -= 1
-            else:
-                return c
+        sum_s = sum(ord(char) for char in s)
+        sum_t = sum(ord(char) for char in t)
+        return chr(sum_t - sum_s)
