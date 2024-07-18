c62aba1168b006c47859604ee2cdd07c815fe50d
mark20306 <71766752+mark20306@users.noreply.github.com>
2024-07-18 12:27:43 +0800

    Time: 37 ms (75.99%), Space: 18.6 MB (12%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -10,7 +10,7 @@ class Solution:
         
         odd = head
         even = head.next
-        even_head = head.next
+        even_head = even
         
         while even and even.next:
             odd.next = even.next
