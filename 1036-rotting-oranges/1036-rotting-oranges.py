60f77b44f83bc88a8aa3ccac27e7449706a9395a
mark20306 <71766752+mark20306@users.noreply.github.com>
2024-07-27 20:35:57 +0800

    Time: 48 ms (71.23%), Space: 16.5 MB (79.83%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -22,11 +22,10 @@ class Solution:
                 if 0 <= r < rows and 0 <= c < cols and grid[r][c] == 1:
                     queue.append([r, c, minute + 1])
                     grid[r][c] = 2
+                    fresh -= 1
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
-                    return -1
+        if fresh > 0:
+            return -1
 
         return minute
 
