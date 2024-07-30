7364f1bbccc0b5124399e9a565fcc1bd7d0ffeb2
mark20306 <71766752+mark20306@users.noreply.github.com>
2024-07-30 20:36:47 +0800

    Time: 36 ms (55.53%), Space: 16.4 MB (52.84%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -3,11 +3,13 @@ class Solution:
         ans = []
         def backtrack(start, numSum, current):
             if len(current) == k and numSum == n:
-                ans.append(current)
+                ans.append(list(current))
                 return
             for i in range(start, 10):
                 if numSum + i <= n:
-                    backtrack(i + 1, numSum + i, current + [i])
+                    current.append(i)
+                    backtrack(i + 1, numSum + i, current)
+                    current.pop()
 
         backtrack(1, 0, [])
         return ans
