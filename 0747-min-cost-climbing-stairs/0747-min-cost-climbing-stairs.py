8014dbda6d855474ac05d1f3ccbcd393713bd53b
mark20306 <71766752+mark20306@users.noreply.github.com>
2024-08-03 04:14:02 +0800

    Time: 56 ms (50.71%), Space: 16.7 MB (58.57%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -6,9 +6,10 @@ class Solution:
         elif n == 1:
             return cost[0]
         
-        dp = [0] * n
-        dp[0] = cost[0]
-        dp[1] = cost[1]
+        first = cost[0]
+        second = cost[1]
         for i in range(2, n):
-            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
-        return min(dp[-1], dp[-2])
+            temp = cost[i] + min(first, second)
+            first = second
+            second = temp
+        return min(first, second)
