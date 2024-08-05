3ff8c419cabf328e68e6539d6fa9c6984094e19f
mark20306 <71766752+mark20306@users.noreply.github.com>
2024-08-05 17:45:32 +0800

    Time: 1192 ms (14.3%), Space: 55.2 MB (44.96%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -3,10 +3,10 @@ class Solution:
         ans = 0
         intervals.sort()
         newEnd = intervals[0][1]
-        for start, end in intervals[1:]:
-            if start < newEnd:
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < newEnd:
                 ans += 1
-                newEnd = min(newEnd, end)
+                newEnd = min(newEnd, intervals[i][1])
             else:
-                newEnd = end
+                newEnd = intervals[i][1]
         return ans
