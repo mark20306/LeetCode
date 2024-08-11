ef0fabb57cdb756736ab03e283f1caaa3b89d948
mark20306 <71766752+mark20306@users.noreply.github.com>
2024-08-11 22:07:52 +0800

    Time: 1539 ms (11.21%), Space: 17.1 MB (36.45%) - LeetHub

--- 1691-minimum-number-of-days-to-disconnect-island/1691-minimum-number-of-days-to-disconnect-island.py
+++ 1691-minimum-number-of-days-to-disconnect-island/1691-minimum-number-of-days-to-disconnect-island.py
@@ -1,8 +1,7 @@
 class Solution:
     def minDays(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        visit = set()
-        count = 0
+
         def dfs(r, c, visit):
             if r < 0 or c < 0 or r == rows or c == cols or grid[r][c] == 0 or (r, c) in visit:
                 return
@@ -10,26 +9,27 @@ class Solution:
             directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
             for dr, dc in directions:
                 dfs(r + dr, c + dc, visit)
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] and (r, c) not in visit:
-                    dfs(r, c, visit)
-                    count += 1
-        if count != 1:
-            return 0
-        land = list(visit)
-        for r, c in land:
-            grid[r][c] = 0
+        
+        def count_island():
             visit = set()
             count = 0
-            for r2 in range(rows):
-                for c2 in range(cols):
-                    if grid[r2][c2] and (r2, c2) not in visit:
-                        dfs(r2, c2, visit)
+            for r in range(rows):
+                for c in range(cols):
+                    if grid[r][c] and (r, c) not in visit:
+                        dfs(r, c, visit)
                         count += 1
-            grid[r][c] = 1
-            if count != 1:
-                return 1
+            return count
+        if count_island() != 1:
+            return 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 0:
+                    continue
+                else:
+                    grid[r][c] = 0
+                    if count_island() != 1:
+                        return 1
+                    grid[r][c] = 1
         return 2
 
 
