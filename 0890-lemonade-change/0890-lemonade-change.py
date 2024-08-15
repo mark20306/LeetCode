8a6d794a4b0a28e4844ab69bc6506adf86104758
mark20306 <71766752+mark20306@users.noreply.github.com>
2024-08-15 13:28:42 +0800

    Time: 619 ms (55.99%), Space: 21.2 MB (15.58%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.py
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -5,11 +5,10 @@ class Solution:
             if i == 5:
                 five += 1
             if i == 10:
-                if five:
-                    five -= 1
-                    ten += 1
-                else:
+                if five == 0:
                     return False
+                five -= 1
+                ten += 1
             elif i == 20:
                 if five and ten:
                     five -= 1
