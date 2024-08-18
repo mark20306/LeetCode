b4285b80a9651b52e6fefd6e260b28978f141c22
mark20306 <71766752+mark20306@users.noreply.github.com>
2024-08-19 00:17:25 +0800

    Time: 84 ms (79.44%), Space: 16.5 MB (71.48%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,13 +1,14 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        minheap = [1]
-        visit = set()
-        factors = [2, 3, 5]
-        for i in range(n):
-            num = heapq.heappop(minheap)
-            if i == n - 1:
-                return num
-            for f in factors:
-                if num * f not in visit:
-                    visit.add(num * f)
-                    heapq.heappush(minheap, num * f)
+        nums = [1]
+        i2, i3, i5 = 0, 0, 0
+        for i in range(1, n):
+            nextNum = min(nums[i2] * 2, nums[i3] * 3, nums[i5] * 5)
+            nums.append(nextNum)
+            if nextNum == nums[i2] * 2:
+                i2 += 1
+            if nextNum == nums[i3] * 3:
+                i3 += 1
+            if nextNum == nums[i5] * 5:
+                i5 += 1
+        return nums[n - 1]
