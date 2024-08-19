e37449a2ebc30d8a8e2dece8c57a127e31a7042a
mark20306 <71766752+mark20306@users.noreply.github.com>
2024-08-19 23:40:41 +0800

    Time: 206 ms (36.67%), Space: 16.6 MB (61.96%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            dp[i] = i
+            for j in range(1, i // 2 + 1):
+                if i % j == 0:
+                    dp[i] = dp[j] + i // j
+        return dp[n]
